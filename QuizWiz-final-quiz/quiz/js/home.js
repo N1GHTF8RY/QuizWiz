5f20a634f93471bed49458fecd450e9d6fe88eab
N1GHTF8RY <abhijithnair0391@gmail.com>
2024-02-02 14:11:09 +0530

    UI Changes

--- QuizWiz-final-quiz/quiz/js/home.js
+++ QuizWiz-final-quiz/quiz/js/home.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
 
-    
+    const signUpPageBtn = $('.user-button.sign-up');
+    const loginPageBtn = $('.log-in');
+
     const startButton = $(".start-button");
     const popupInfo = $(".popup-info");
     const main = $(".main")
-       
+    
+    
+    loginPageBtn.click(function(){
+        window.localStorage.setItem("isSignUp", "false");
+        window.location.assign("signup.html");
+        window.location.reload();
+    });
+
+    signUpPageBtn.click(function(){
+        window.localStorage.setItem("isSignUp", "true");
+        window.location.assign("signup.html");
+        window.location.reload();
+    });
+
     startButton.click(function(){
         popupInfo.addClass("active");
         main.addClass("active");
@@ -29,11 +44,6 @@ $(document).ready(function(){
     closeButton.click(() => {
         slidingMenu.removeClass("show");
     });
-
-    
-
-
-
 
 
 })
